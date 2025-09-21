f06454ea567e679ea3f366e62e66d686d55123ab
塩/Salt <nabb9771@icloud.com>
2025-09-21 21:07:20 +0900

    Add files via upload

---
+++ data/lang.js
@@ -0,0 +1,18 @@
+const lang = {
+    'リアルタイム速度：': 'Live Speed:',
+    '回/秒': ' times/s',
+    '最大速度：': 'Max Speed:',
+    '正打：': 'correct:',
+    '誤打：': 'wrong:',
+    '正誤率：': 'accuracy:',
+
+    '難易度': 'difficulty',
+    'フォント': 'font',
+    'タイプ音': 'type sound',
+    '言語設定': 'language',
+    '現在の秒数は': 'current number of seconds is',
+    '秒です': 's',
+    '問題を追加する：': 'Add My Questions',
+    '※漢字を含む': 'alphabet only',
+    '回': ' times',
+};
